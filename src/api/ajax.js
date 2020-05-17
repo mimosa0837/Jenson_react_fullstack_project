e9f0a3254aef65dd8264f3d934ff1686272c6b2b
JiashengWang <w25726202781@163.com>
2020-05-18 01:33:01 +0800

    fix bug

---
+++ src/api/ajax.js
@@ -0,0 +1,39 @@
+// 能发送异步ajax请求的函数模块
+// 封装axios库
+// 函数的返回值是promise对象  ===> 后面用上async/await
+
+// 1. 内部统一处理请求异常: 外部的调用都不用使用try..catch来处理请求异常
+// 2. 异步返回是响应数据(而不是响应对象): 外部的调用异步得到的就直接是数据了(response --> response.data)
+
+import axios from 'axios'
+import {message} from 'antd'
+
+export default function ajax(url, data={}, type='GET') {
+
+  return new Promise((resolve, reject) => {
+    let promise
+    // 1. 执行异步ajax请求
+    if(type==='GET') { // 发GET请求
+      promise = axios.get(url, { // 配置对象
+        params: data // 指定请求参数
+      })
+    } else { // 发POST请求
+      promise = axios.post(url, data)
+    }
+    // 2. 如果成功了, 调用resolve(value)
+    promise.then(response => {
+      resolve(response.data)
+    // 3. 如果失败了, 不调用reject(reason), 而是提示异常信息
+    }).catch(error => {
+      // reject(error)
+      message.error('请求出错了: ' + error.message)
+    })
+  })
+
+
+}
+
+// 请求登陆接口
+// ajax('/login', {username: 'Tom', passsword: '12345'}, 'POST').then()
+// 添加用户
+// ajax('/manage/user/add', {username: 'Tom', passsword: '12345', phone: '13712341234'}, 'POST').then()
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// 入口index
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './redux/store'
+import App from './App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 将App组件标签渲染到index页面的div上
+ReactDOM.render((
+  // 向所有的容器组件提供store
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'))
